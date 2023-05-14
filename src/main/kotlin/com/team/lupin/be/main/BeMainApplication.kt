96a8df36dc24d073e1791814463cf0ca157b3296
USER <user@192.168.nate.com>
2023-05-14 12:23:32 +0900

    init

---
+++ src/main/kotlin/com/team/lupin/be/main/BeMainApplication.kt
@@ -0,0 +1,11 @@
+package com.team.lupin.be.main
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BeMainApplication
+
+fun main(args: Array<String>) {
+	runApplication<BeMainApplication>(*args)
+}
